d1523064066ef16d1c5b5648c1780c7e77d17478
kapoorshubham1993 <shubhamkapoor93@gmail.com>
2023-07-28 18:42:12 +0530

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Halthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+
